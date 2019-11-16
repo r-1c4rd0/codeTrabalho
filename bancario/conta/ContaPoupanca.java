6868d2d6dc578c3fbc4ad2d0a943040c2597bd97
Ricardo Santiago <ricardosantiagocwb@gmail.com>
2019-11-16 16:09:03 -0300

    mod 1

--- bancario/conta/ContaPoupanca.java
+++ bancario/conta/ContaPoupanca.java
@@ -9,22 +9,40 @@ package trabalho.faculdade.unicesumar.bancario.conta;
  *
  * @author Ricardo_Santiago
  */
-public class ContaPoupanca extends Conta {
+
+public class ContaPoupanca extends Conta{
 
     public ContaPoupanca(int a, int n, int d, double s) {
-        super(a, n, d, s);
-        rendimento(0.02);
+        super(a, n, d, s);        
+        rendmento(0.02);
     }
-
-    
-    public void rendimento(double perc) {
-        double rendimento = this.getSaldo() * perc;
-        this.setSaldo(this.getSaldo() + rendimento);
-    }
-
+   
     @Override
-    public String toString() {
-        return " \tPOUPANÇA:::\n";
-
+    public void setSaldo(double s){        
+        if(this.saldo<0){
+            System.out.println("Valor não permitido!!!");
+        }else{
+            this.saldo=s;
+        }
+            
+    }
+    
+    @Override
+    public void saque(double saque){
+        if(this.getSaldo() >= 0.0 && saque <= this.getSaldo()){
+            this.setSaldo(this.getSaldo() - saque);            
+        }else{
+            System.out.println(" Saldo insulficiente ");
+        }        
+    }   
+    public void rendmento(double perc){ 
+      double rendimento = this.getSaldo() * perc;
+      this.setSaldo(this.getSaldo() + rendimento); 
+      
+   } 
+   @Override
+    public String toString(){
+              return " \tPOUPANÇA:::\n";
+        
     }
 }
